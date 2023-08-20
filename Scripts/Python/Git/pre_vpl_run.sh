6891049a2274c41ede7485ee821b729738999e51
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-21 00:59:32 +0300

    Update pre_vpl_run.sh

--- Scripts/Python/Git/pre_vpl_run.sh
+++ Scripts/Python/Git/pre_vpl_run.sh
@@ -1,11 +1,12 @@
 student_files="student_folder"
 requested_file="link_py.txt" # Name of the file containing student inputt
 
+commit_teacher="8685265"
+teacher_link="https://github.com/TalMalchi/Ex8-Python.git" # Link to teacher's git repository
 # Read the student's input file and extract the repository URL and commit number
 mapfile -t input_lines < "$requested_file"
 repo_url="${input_lines[0]}"
 commit="${input_lines[1]}"
-
 # Clone the student's repository
 git clone --quiet "$repo_url" "$student_files"
 
@@ -14,5 +15,11 @@ cd "$student_files"
 git checkout "$commit"
 mv ./* ..
 cd ..
-rm -rf "$student_files"
+#ls -l 
+
+git clone --quiet "$teacher_link" "$teacher_files"
+cd "$teacher_files"
+git checkout "$commit_teacher"
+mv ./* ..
+cd ..
 
