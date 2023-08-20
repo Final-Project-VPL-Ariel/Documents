b03180c3c4927db26bfacb190a888b5c61722039
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-20 18:16:14 +0300

    Update pre_vpl_run.sh

--- Scripts/Python/Git/pre_vpl_run.sh
+++ Scripts/Python/Git/pre_vpl_run.sh
@@ -4,25 +4,19 @@ requested_file="link_py.txt" # Name of the file containing student inputt
 # Read the student's input file and extract the repository URL and commit number
 mapfile -t input_lines < "$requested_file"
 repo_url="${input_lines[0]}"
-commit_number="${input_lines[1]}"
+commit="${input_lines[1]}"
 
 # Clone the student's repository
 git clone --quiet "$repo_url" "$student_files"
 
 # Move to the student's repository directory
 cd "$student_files"
+git checkout "$commit"
+cd ..
 
-# Check if the provided commit number matches the actual commit number in the student's repository
-actual_commit_number=$(git rev-parse --short=7 HEAD)
-echo $actual_commit_number
-echo $commit_number
-if [[ "$commit_number" == "$actual_commit_number" ]]; then
+
     echo "The student's repository has the desired commit number: $commit_number"
     mv ./* ..
     cd ..
     rm -rf "$student_files"
-else
-    echo "The student's repository does not have the desired commit number: $commit_number"
-    cd ..
-    rm -rf "$student_files"
-fi
+
