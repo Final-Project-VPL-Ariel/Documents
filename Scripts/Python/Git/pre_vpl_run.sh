31e994f5b6df1db90ddc462366d78a3e4745390c
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-26 18:37:45 +0300

    Update pre_vpl_run.sh

--- Scripts/Python/Git/pre_vpl_run.sh
+++ Scripts/Python/Git/pre_vpl_run.sh
@@ -1,22 +1,30 @@
 student_files="student_folder"
-requested_file="link_py.txt" # Name of the file containing student inputt
+teacher_files="teacher_folder"
+requested_file="link_py.txt" #must be change to the same name in the requested file
 
-commit_teacher="8685265"
+commit_teacher="3cac178"
 teacher_link="https://github.com/TalMalchi/Ex8-Python.git" # Link to teacher's git repository
+
 # Read the student's input file and extract the repository URL and commit number
 mapfile -t input_lines < "$requested_file"
 repo_url="${input_lines[0]}"
 commit="${input_lines[1]}"
 # Clone the student's repository
+
 git clone --quiet "$repo_url" "$student_files"
-sleep 5 
 # Move to the student's repository directory
+sleep 5 
+cd "$student_files" 
 git checkout "$commit"
-mv $student_files/* ./
+mv ./* ..
+cd ..
+rm -f -r "$student_files"
+rm -f -r "$teacher_files"
 
 git clone --quiet "$teacher_link" "$teacher_files"
 sleep 5 
+cd "$teacher_files"
 git checkout "$commit_teacher"
-mv $teacher_files/* ./
-
-
+# move all the files to main directory ecxept  __pycache__
+find . -maxdepth 1 -type f ! -path "./__pycache__/*" -exec mv {} .. \;
+cd ..
