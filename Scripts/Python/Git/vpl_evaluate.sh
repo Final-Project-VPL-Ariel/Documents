fe892a9e039f1cfd5a69bacc74ad217bce9135be
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-26 19:25:29 +0300

    Update vpl_evaluate.sh

--- Scripts/Python/Git/vpl_evaluate.sh
+++ Scripts/Python/Git/vpl_evaluate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bash pre_vpl_run.sh
-
-python3 -m unittest discover -v -b -p *_test.py >> ans.txt 2>&1
+echo "----"
+ls -l 
+python3  test_*.py >> ans.txt 2>&1
 echo "#!/bin/bash" >> vpl_execution
 
 echo "echo '<|--'" >> vpl_execution
@@ -35,7 +36,7 @@ echo $grade
 
 #-------------------------------------
 grade=$(./round_grade.sh $grade)
-python3 -m unittest discover -v -p *_test.py >> ans.txt 2>&1
+python3 test_*.py >> ans.txt 2>&1
 if [ $? -eq 0 ]; then
     echo "All tests passed!"
     echo "echo 'Grade :=>> 100'" >> vpl_execution
